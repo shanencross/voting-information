8f9b4c60aa042e50c693b9d843a1ded650549421
Shanen Cross <shanencross@yahoo.com>
2021-01-27 20:20:16 -0800

    Fix to check age greater than OR EQUAL to 18

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $("document").ready(function() {
 	const userAge = prompt("How old are you?");
 
-	if (userAge > 18) {
+	if (userAge >= 18) {
 		$("#of-voting-age").show();
 	} else {
 		$("#under-voting-age").show();12
